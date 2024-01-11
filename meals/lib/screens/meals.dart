116c1da7f1853e218193b6d80607f377a0e2a966
z1nl0x <paulo.kreft@gmail.com>
2024-01-10 23:10:44 -0300

    Meals App updates

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 
@@ -10,11 +9,43 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh...nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: ListView.builder(itemBuilder: (ctx, index) => Text(meals[index].title)),
-    )
+      body: content,
+    );
   }
 }
