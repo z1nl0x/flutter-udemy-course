2eea4ec704ed9a2882c54f9084366327761e033d
z1nl0x <paulo.kreft@gmail.com>
2024-01-13 14:05:34 -0300

    Meals App updates

--- meals/lib/widgets/category_grid_item.dart
+++ meals/lib/widgets/category_grid_item.dart
@@ -5,15 +5,17 @@ class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
     required this.category,
+    required this.onSelectCategory,
   });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        
+        onSelectCategory();
       },
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
