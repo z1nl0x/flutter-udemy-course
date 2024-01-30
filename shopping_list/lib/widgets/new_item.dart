610b425fa737ac19b023ce3a2f00c4a01edc6e29
z1nl0x <paulo.kreft@gmail.com>
2024-01-29 21:45:07 -0300

    Shopping List App updates

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
@@ -20,18 +22,39 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
-      final url = Uri.https('flutter-prep-689ec-default-rtdb.firebaseio.com', 'shopping-list.json');
-      http.post(url);
+      final url = Uri.https('flutter-prep-689ec-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title
+        }),
+      );
 
-      Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory));
+      //   Navigator.of(context).pop(GroceryItem(
+      //       id: DateTime.now().toString(),
+      //       name: _enteredName,
+      //       quantity: _enteredQuantity,
+      //       category: _selectedCategory));
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
