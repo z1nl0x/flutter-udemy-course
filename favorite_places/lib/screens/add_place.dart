37dfaf1f23757d6f0555f348b5cc1b86c24ca30f
Paulo Kreft <paulo.kreft@gmail.com>
2024-02-14 17:17:38 -0300

    Favorite Places App updates

--- favorite_places/lib/screens/add_place.dart
+++ favorite_places/lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -49,12 +50,17 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
             const SizedBox(
+              height: 10,
+            ),
+            ImageInput(),
+            const SizedBox(
               height: 16,
             ),
             ElevatedButton.icon(
-                onPressed: _savePlace,
-                icon: const Icon(Icons.add),
-                label: const Text('Add Place'))
+              onPressed: _savePlace,
+              icon: const Icon(Icons.add),
+              label: const Text('Add Place'),
+            ),
           ],
         ),
       ),
---
+++ favorite_places/lib/widgets/image_input.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({Key? key}) : super(key: key);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+
+  void _takePicture() {
+    print('HELLO PICTURE!');
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: _takePicture,
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+      ),
+    );
+  }
+}
